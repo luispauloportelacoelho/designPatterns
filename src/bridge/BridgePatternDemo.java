03e236998e8ac32eb8f4f6407967e2d034262c32
Luís Coelho <luisportelacoelho@gmail.com>
2019-05-19 23:33:54 +0100

    Bridge pattern version 1.

---
+++ src/bridge/BridgePatternDemo.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public class BridgePatternDemo {
+    public static void main(String[] args) {
+        Shape shape1 = new Circle(100, 100, 10, new RedCircle());
+        Shape shape2 = new Circle(200, 200, 20, new GreenCircle());
+
+        shape1.draw();
+        shape2.draw();
+    }
+}
