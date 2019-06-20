f014d9f899f843df35bc7bc9ec676d3342efccf9
Luís Coelho <luisportelacoelho@gmail.com>
2019-06-21 00:50:32 +0100

    Different approach of state3

---
+++ src/state/state3/test/Client.java
@@ -0,0 +1,16 @@
+package state.state3.test;
+
+public class Client {
+    public static void main(String[] args){
+        Atm atm = new Atm();
+
+        atm.refill(100);
+        atm.withdraw(50);
+        atm.withdraw(30);
+        atm.withdraw(50);
+        atm.withdraw(50);
+        atm.withdraw(40);
+        atm.refill(200);
+        atm.withdraw(10);
+    }
+}
