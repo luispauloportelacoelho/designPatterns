9ba3530bf03f766008699d033496335bfc763d89
Luís Coelho <luisportelacoelho@gmail.com>
2019-06-29 18:38:36 +0100

    Adapter pattern: third example

---
+++ src/adapter/adapter3/after/LineAdapter.java
@@ -0,0 +1,15 @@
+package adapter.adapter3.after;
+
+public class LineAdapter implements Shape {
+
+    private Line adaptee;
+
+    public LineAdapter(Line line) {
+        this.adaptee = line;
+    }
+
+    @Override
+    public void draw(int x1, int x2, int y1, int y2) {
+        adaptee.draw(x1, x2, y1, y2);
+    }
+}
